8b0cbaf28a05f089d941331778f3617d7c89613e
ByAhmetM <118487365+ByAhmetM@users.noreply.github.com>
2022-12-25 02:11:54 +0300

    son

--- src/components/root/App.js
+++ src/components/root/App.js
@@ -1,9 +1,15 @@
 import React from "react";
-import Dashboard from "./Dashboard"
-
+import CategoryList from "../categories/CategoryList";
+import ProductList from "../products/ProductList";
+import { Row, Col } from "reactstrap";
 function App() {
   return <div>
-  <Dashboard/>
+    <div>
+      <Row>
+        <Col xs="3"><CategoryList/></Col>
+        <Col xs="9"><ProductList/></Col>
+      </Row>
+    </div>
   </div>;
 }
 
